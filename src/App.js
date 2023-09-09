74592fea102c9f4f5b114217e8510e60ea50f88d
Debarshee Chakraborty <129538241+debarshee2004@users.noreply.github.com>
2023-09-09 13:06:05 +0000

    Creating the main body

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import "./styles.css";
+import logo from "./assets/font.png";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="Background">
+      <form>
+        <img src={logo} alt="Logo" width="200px" />
+        <div className="input">
+          <label>Name:</label>
+          <input type="text" id="fname" />
+        </div>
+        <div className="input">
+          <label>Email:</label>
+          <input type="text" id="fname" />
+        </div>
+        <div className="input">
+          <label>Contact:</label>
+          <input type="text" id="fname" />
+        </div>
+        <button>
+          Make Payment
+        </button>
+      </form>
     </div>
   );
 }
